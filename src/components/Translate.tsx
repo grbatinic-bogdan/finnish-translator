41553b8203e5cb37e2a3316deda9a5f10e5833da
Bogdan Grbatinic <grbatinic.bogdan@gmail.com>
2020-06-07 14:00:06 +0300

    Add material ui dependancies, add material ui components, styling and fonts. Restyle translation feature

--- src/components/Translate.tsx
+++ src/components/Translate.tsx
@@ -1,16 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { ITranslateWord } from 'src/api/actions';
+import { Typography, TextField, makeStyles, Box } from '@material-ui/core';
+import CheckIcon from '@material-ui/icons/Check';
+import { SvgIcon } from '@material-ui/core';
 
 interface ITranslateProps {
   translate: ITranslateWord;
 }
 
+const useStyles = makeStyles({
+  inputAlignment: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  label: {
+    padding: '1rem 0',
+  },
+});
+
 export const Translate: React.FunctionComponent<ITranslateProps> = ({ translate }) => {
+  const classes = useStyles();
+
   const [translationValue, setTranslationValue] = useState('');
   const [isTranslated, setIsTranslated] = useState(false);
+  const [errorCounter, setErrorCounter] = useState(0);
+
   const { translationValues, baseLanguageValue } = translate;
   const hiddenTranslationValue = translationValues[0];
-  const [errorCounter, setErrorCounter] = useState(0);
 
   useEffect(() => {
     setTranslationValue('');
@@ -40,20 +56,31 @@ export const Translate: React.FunctionComponent<ITranslateProps> = ({ translate 
   };
 
   return (
-    <>
-      <p>Translate: {baseLanguageValue}</p>
-      <div>
-        <input
-          type="text"
-          placeholder="translation value"
-          name="translationValue"
-          value={translationValue}
-          onChange={handleInputChange}
-          disabled={isTranslated}
-          className={`ba ${isTranslated ? 'b--green' : 'b--light-silver'}`}
-        />
-        <p>Number of errors you made {errorCounter}</p>
-      </div>
-    </>
+    <Box>
+      <Typography>
+        <Box className={classes.label}>
+          Word to translate:{' '}
+          <Typography display="inline" noWrap={true} color="primary">
+            {baseLanguageValue}
+          </Typography>
+        </Box>
+        <Box className={classes.inputAlignment}>
+          <TextField
+            value={translationValue}
+            onChange={handleInputChange}
+            disabled={isTranslated}
+            label="Translation"
+            fullWidth
+          />
+          {isTranslated && <SvgIcon component={CheckIcon} />}
+        </Box>
+        <Box className={classes.label}>
+          Number of errors you made{' '}
+          <Typography display="inline" color={errorCounter > 0 ? 'error' : 'initial'}>
+            {errorCounter}
+          </Typography>
+        </Box>
+      </Typography>
+    </Box>
   );
 };
